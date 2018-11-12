42e785dcbad1025bb04f89f5cbacb1d67322836b
David Roundy <roundyd@physics.oregonstate.edu>
2018-11-12 06:54:25 -0800

    start adding DisplayAs trait

--- display-as/src/lib.rs
+++ display-as/src/lib.rs
@@ -1,3 +1,27 @@
+#![deny(missing_docs)]
+
+//! This crate defines a trait `DisplayAs` that allows a type to be
+//! displayed in a particular format.
+
+use std::fmt::{Formatter, Error};
+
+/// Format is a format that we can use for displaying data.
+pub trait Format {
+    /// "Escape" the given string so it can be safely displayed in
+    /// this format.  The precise meaning of this may vary from format
+    /// to format, but the general sense is that this string does not
+    /// have any internal formatting, and must be displayed
+    /// appropriately.
+    fn escape(&self, &str) -> String;
+}
+
+/// This trait is analogous to `Display`, but will display the data in
+/// `F` format.
+pub trait DisplayAs<F: Format> {
+    /// Formats the value using the given formatter.
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error>;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![deny(missing_docs)]
+
+//! This will be a template library that uses `display-as`.
+
 #[cfg(test)]
 mod tests {
     #[test]
