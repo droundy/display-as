22aeace04aee3913e9578ff8ab1641045d37709a
David Roundy <roundyd@physics.oregonstate.edu>
2018-11-12 21:02:16 -0800

    start working on compile_templates

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,25 @@
 
 //! This will be a template library that uses `display-as`.
 
+extern crate display_as;
+extern crate glob;
+
+/// Use this function in your `build.rs` to compile templates.
+pub fn compile_templates(files_glob: &str) -> std::io::Result<()> {
+    for path in glob::glob(files_glob).expect("unable to read template directory") {
+        if let Ok(path) = path {
+            let template = std::fs::read_to_string(&path)?;
+            println!("template {:?}: {}", &path, template);
+        }
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
+    use super::compile_templates;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        compile_templates("tests/foo.html").unwrap();
     }
 }
