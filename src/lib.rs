d8a66ded35295433bb91f2a7142455b049047908
David Roundy <roundyd@physics.oregonstate.edu>
2018-11-19 07:32:08 -0800

    drafty version, need to move tests to separate crate

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub extern crate display_as;
 extern crate glob;
 #[macro_use]
 extern crate combine;
+extern crate regex;
 
 use display_as::{As, Rust};
 use std::fs::File;
@@ -13,6 +14,7 @@ use std::io::Write;
 use std::path::{Path, PathBuf};
 
 mod parse;
+mod rust;
 
 fn add_rs_extension(p: &Path) -> PathBuf {
     let mut p = p.to_string_lossy().to_string();
@@ -37,6 +39,41 @@ pub fn compile_templates(files_glob: &str) -> std::io::Result<()> {
     Ok(())
 }
 
+/// Use this function in your `build.rs` to compile templates.
+pub fn compile_all_templates() -> std::io::Result<()> {
+    let mut sources: Vec<_> = glob::glob("src/**/*.rs").expect("unable to read src directory").collect();
+    sources.extend(glob::glob("tests/**/*.rs").expect("unable to read src directory"));
+    for rs in sources {
+        if let Ok(rs) = rs {
+            let code = std::fs::read_to_string(&rs)?;
+            println!("Looking inside {:?}", rs);
+            for r in rust::parse_rust(&code) {
+                println!("looking at {:?}", r);
+                let reversed_name: String = r.template_name.chars().rev().collect();
+                let template_name: String =
+                    reversed_name.replacen("_", ".", 1).chars().rev().collect();
+                let template_name = template_name.replace(".rs", "");
+                let mut template_path = PathBuf::from(rs.parent().unwrap());
+                template_path.push(&template_name);
+                println!("reading file {:?}", &template_path);
+                let template = std::fs::read_to_string(&template_path)?;
+                let tt = parse::parse_template(&template);
+                let mut newcode_path = PathBuf::from(rs.parent().unwrap());
+                newcode_path.push(&r.template_name);
+                println!("about to create {:?}", &newcode_path);
+                let mut f = File::create(&newcode_path)?;
+                writeln!(f, "{} {{", r.impl_text);
+                writeln!(f, "  fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {{");
+                for t in tt.iter() {
+                    writeln!(f, "{}", As(Rust, t.clone()));
+                }
+                writeln!(f, "Ok(()) }}\n}}");
+            }
+        }
+    }
+    Ok(())
+}
+
 #[macro_export]
 macro_rules! display_as_with_template {
     ($format:ident, $type:ty, $template_name:tt) => {
@@ -50,11 +87,22 @@ macro_rules! display_as_with_template {
     }
 }
 
+#[macro_export]
+macro_rules! display_as_template {
+    ($format:ident; $impl:stmt; $template_name:tt) => {
+        include!($template_name);
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::compile_templates;
+    use super::{compile_templates, compile_all_templates};
     #[test]
     fn compile_foo_template() {
         compile_templates("templates/foo.html").unwrap();
+    }
+    #[test]
+    fn test_compile_all_templates() {
+        compile_all_templates().unwrap();
     }
 }
