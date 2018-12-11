a73b3e0b2e26152780692dbd10dfc7c0674b6482
David Roundy <roundyd@physics.oregonstate.edu>
2018-12-11 09:52:04 -0800

    test for another hypothesis for the bug I am seeing elsewhere

--- display-as/tests/mixed-types.rs
+++ display-as/tests/mixed-types.rs
@@ -5,3 +5,13 @@ fn mixed_types() {
     assert_eq!(&format_as!(HTML, "hello world " 5 as URL " urls are " 5.0),
                "hello world 5 urls are 5");
 }
+
+#[test]
+fn mixed_formats_in_let() {
+    assert_eq!(&format_as!(HTML,
+                           let foo = {
+                               "hello world " 5 as URL " urls are " 5.0
+                           };
+                           foo),
+               "hello world 5 urls are 5");
+}
